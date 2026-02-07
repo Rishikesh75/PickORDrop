d72875e76857fa552b3215bec6fedaa08485a327
Rishikesh27 <kotlarishikesh27@gmail.com>
2026-02-06 23:16:18 -0800

    Initial Commit

---
+++ PickORDrop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PickORDrop
+//
+//  Created by Rishi on 06/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PickORDrop/PickORDropApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickORDropApp.swift
+//  PickORDrop
+//
+//  Created by Rishi on 06/02/26.
+//
+
+import SwiftUI
+
+@main
+struct PickORDropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
